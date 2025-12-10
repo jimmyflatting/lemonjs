fda5ece7a7957ba9fa0aa07c397cf7eed1ef4c27
Jimmy Flatting <jimmy.flatting@icloud.com>
2025-12-10 14:53:49 +0100

    chore: run formatting

--- lib/lemon.ts
+++ lib/lemon.ts
@@ -3,6 +3,6 @@ var server = require("./server");
 exports = module.exports = {};
 
 function createApp() {
-    var app = {};
-    return {};
+  var app = {};
+  return {};
 }
--- lib/server.ts
+++ lib/server.ts
@@ -1,3 +1,3 @@
 var http = require("http");
 
-console.log("Hello World!")
+console.log("Hello World!");
